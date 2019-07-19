ce9cccf7e92694690b79ebaa35798fc01195b076
bianjiashuai <393252841@qq.com>
2019-07-19 13:53:13 +0800

    练习代码

---
+++ src/main/java/com/bjs/session/SessionApplication.java
@@ -0,0 +1,16 @@
+package com.bjs.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author BianJiashuai
+ */
+@SpringBootApplication
+public class SessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionApplication.class, args);
+    }
+}
